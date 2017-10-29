b850b89fa6cb79cf3e076d19cac86f668e75994b
SakuraM <h24i18@ed.cc.suzuka-ct.ac.jp>
2017-10-29 23:00:28 +0900

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,9 @@ Rails.application.configure do
 
 
 #config.action_mailer.default_url_options = { host: 'glacial-coast-32397.herokuapp.com' }
-#config.action_mailer.default_url_options = { host: 'glacial-coast-32397.herokuapp.com' }
+config.action_mailer.default_url_options = { host: 'glacial-coast-32397.herokuapp.com' }
 
-config.action_mailer.default_url_options = { host: '52.198.150.126' }
+#config.action_mailer.default_url_options = { host: '52.198.150.126' }
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings =
 {
