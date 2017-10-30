48f66005ee32f50638397597be947a79b251d412
SakuraM <h24i18@ed.cc.suzuka-ct.ac.jp>
2017-10-31 00:50:47 +0900

    UI

--- app/models/up.rb
+++ app/models/up.rb
@@ -2,6 +2,7 @@ class Up < ActiveRecord::Base
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true , length: { minimum: 1 , maximum: 400 }
   belongs_to :user
+  validates :photo, presence: true
  # CommentモデルのAssociationを設定
  has_many :comments, dependent: :destroy
   mount_uploader :photo, PhotoUploader
